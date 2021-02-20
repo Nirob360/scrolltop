c1697b84dce5acedb6d72a8114f45a40b675a1b9
md minarul islam <57551661+Nirob360@users.noreply.github.com>
2021-02-20 18:59:08 +0600

    scrolltop jquery coede

---
+++ scrolltop.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function () {
+    
+
+
+
+    $('a[href=#top]').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.totop a').fadeIn();
+        } else {
+            $('.totop a').fadeOut();
+        }
+    });
+
+
+});
+
